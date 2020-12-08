77c5adeaf25c0f827ae756d9e7f22eea02978b2f
Peter Vieira <pete.vieira@languageconservancy.org>
2020-12-08 01:19:24 -0500

    update. scroll slash jump to in-page link issue

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,5 @@
 let scrollDown;
 let lastScrollTop = 0;
-let lastId = "";
-let sections = document.querySelectorAll(['h1[id]','h2[id]']);
 
 window.addEventListener("scroll", function() {
   let st = window.pageYOffset || document.documentElement.scrollTop;
@@ -12,7 +10,6 @@ window.addEventListener("scroll", function() {
 function getIndex(id) {
   let sections = document.querySelectorAll(['h1[id]','h2[id]']);
   for (let i = 0; i < sections.length; ++i) {
-    console.log(i + ": " + sections[i].id);
     if (sections[i].id == id) {
       return i;
     }
@@ -25,41 +22,32 @@ window.addEventListener('DOMContentLoaded', () => {
   let tocEls = document.querySelectorAll(`.table-of-contents ul a`);
   for (let i = 0; i < els.length; ++i) {
     const tag = els[i].tagName.toLowerCase();
-    els[i].id = tag+"-"+i;
+    els[i].id = tag+"-"+i; // anti-scroll offending line
     tocEls[i].id = tag+"-"+i;
   }
 
   let lastItems = [];
   const observer = new IntersectionObserver(entries => {
+    let tocEls = document.querySelectorAll(`.table-of-contents ul a`);
+    tocEls.forEach(tocEl => {
+      tocEl.classList.remove('active');
+    });
+    // let el = document.querySelector(`.table-of-contents ul a[id*="${id}"]`);
     for (let i = 0; i < entries.length; ++i) {
       let entry = entries[i];
-      // entries.forEach(entry => {
       const id = entry.target.getAttribute('id');
-      console.log("id: " + id);
+      let el = document.querySelector(`.table-of-contents ul a[id*="${id}"]`);
 
       if (entry.intersectionRatio > 0) {
-        lastId = id;
-        let el = document.querySelector(`.table-of-contents ul a[id*="${id}"]`);
         el?.classList.add('active');
       } else {
-        let el = document.querySelector(`.table-of-contents ul a[id*="${id}"]`);
         el?.classList.remove('active');
-        if (entries.length > 0) {
-          return;
-        }
-        console.log("lastId: " + lastId);
-        let index = getIndex(id);
-        console.log(index);
-        if (index < 0) {
-          return;
-        }
-        let newIndex = Math.max(scrollDown ? i : i-1, 0);
-        let sectionss = document.querySelectorAll(['h1[id]','h2[id]'])
-        let newEntry = sectionss[newIndex];
-        let newId = newEntry.id;
-        el = document.querySelector(`.table-of-contents ul a[id*="${id}"]`);
-        el?.classList.add('active');
+      }
 
+      if (entries.length == 1 && entry.intersectionRatio == 0) {
+        let ii = getIndex(id);
+        let index = Math.max(scrollDown ? ii : ii-1, 0);
+        let newId = tocEls[index].classList.add('active');;
       }
     }
 
